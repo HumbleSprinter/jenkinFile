3d7a3d1739f89c28f37f8a8a252ab2c523d88272
HumbleSprinter <43790128+HumbleSprinter@users.noreply.github.com>
2021-08-10 16:02:49 +0530

    Create class1.java

---
+++ class1.java
@@ -0,0 +1,16 @@
+package day6;
+
+public class jdbc{
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		
+		
+		
+		
+	}
+
+}
